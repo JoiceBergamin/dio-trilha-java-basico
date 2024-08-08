31f2ae8d152ef1ef10a679c6126d7ac7c299e58d
Joice Bergamin <joicenafef@gmail.com>
2024-08-08 14:13:08 -0300

    Código do desafio finalizado

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,31 @@
+import java.util.Locale;
+import java.util.Scanner; //Classe Scanner importada
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner ler = new Scanner(System.in).useLocale(Locale.US); //objeto "ler" da classe scanner
+
+       
+        int numero = 0;
+        String agencia;
+        String nomeCliente;
+        double saldo = 0;
+
+        System.out.println("BEM VINDO AO NOSSO SISTEMA DE CADASTRO BANCÁRIO!");
+        System.out.println("Por favor, insira o numero da agencia:");
+        agencia = ler.next();
+        System.out.println("Por favor, insira o numero da conta:");
+        numero = ler.nextInt();
+        ler.nextLine();
+        System.out.println("Por favor, insira nome completo:");
+        nomeCliente = ler.nextLine();
+        System.out.println("Por favor, insira o valor do saldo para depósito:");
+        saldo = ler.nextDouble();
+
+
+        System.out.println("Olá " + nomeCliente + " obrigado por criar uma conta em nosso banco, sua agência é "
+         + agencia + ", conta " + numero + " e seu saldo de R$" + saldo +
+        " já está disponível para saque.");
+        System.out.println("Obrigado por confiar em nossos serviços!");
     }
 }
